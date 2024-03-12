f313429b8d25b5b3b4ad1654e6901c1c363dca1e
MelindaN14 <137628431+MelindaN14@users.noreply.github.com>
2024-03-11 21:15:24 -0400

    alerts

--- block07/app.js
+++ block07/app.js
@@ -4,3 +4,8 @@ add comments throughout code
 crate dialog box displaying vault codes and the text with html
 */
 console.log("Unlock the Vault");
+alert("Unlock the Vault");
+console.log("Combination # 1");
+alert("20-10");
+// Used basic math //
+
