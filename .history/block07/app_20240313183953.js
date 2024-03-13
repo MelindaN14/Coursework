9683c40bf788b656df8ee0591a0a7fd7cd4a2813
MelindaN14 <137628431+MelindaN14@users.noreply.github.com>
2024-03-13 18:51:35 -0400

    new

---
+++ .history/block07/app_20240313183953.js
@@ -0,0 +1,23 @@
+/*Create a string 
+assign three variables
+add comments throughout code
+crate dialog box displaying vault codes and the text with html
+combination 10-40-39
+*/
+//Combination Codes in alert pop-up.//
+
+const message =
+  "You have received this message because you have been chosen to open an important vault.";
+const vault = "Here is the secret combination:";
+console.log(`please read ${message}${vault}`); 
+//can be seen via inspect//
+
+console.log("Unlock the Vault");
+alert("Unlock the Vault");
+console.log("Combination #1");
+alert(20 - 10);
+// Used basic math but it is solving it //
+console.log("Combination #2");
+alert(20 * 2);
+console.log("Combination #3");
+alert(78 / 2);
