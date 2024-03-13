98eba48d954335cb7ce195d083c8264db0d9f548
MelindaN14 <137628431+MelindaN14@users.noreply.github.com>
2024-03-12 21:15:49 -0400

    no

---
+++ .history/block07/index_20240312210348.js
@@ -0,0 +1,40 @@
+// alert("hello i see you");
+console.log("Hello World");
+function helloworld() {
+  console.log("say my name!");
+}
+/*comment syntax*/
+// makes a line of code not valid//
+// command+forward slash for quick key to comment out code//
+// Pseudocode: example : Create a dialog box that says Hello//
+let x = "hello world";
+let myNumber = 2;
+let thisIsAdog = true;
+let myCar = "Toyota";
+myCar = "BMW";
+console.log("myCar");
+console.log("myCar");
+
+let thisIsNull = null;
+let thisIsUndefined = undefined;
+let thisIsANumber = 1;
+let thisIsAString = "Hello";
+let thisIsABoolean = true;
+
+console.log(Boolean);
+
+// let can be changed const should not //
+
+const firstName = "Melinda";
+const lastName = "Nanjad";
+// console.log("this is my name:"firstName + " " + lastName);
+
+// string literal
+
+console.log(`this is my name: ${firstName} ${lastName}`);
+
+console.log(5-2)
+console.log(10-4)
+console.log(2>=2) 
+console.log('a'>'b')
+<!--added .js file at the bottom because it needs to render after the html etc-->
