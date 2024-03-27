9e2dacce3c2b394fd457da8f7e5d7c0febc81ba4
MelindaN14 <137628431+MelindaN14@users.noreply.github.com>
2024-03-27 19:32:14 -0400

    new filesss

---
+++ .history/block13/index_20240327192353.js
@@ -0,0 +1,20 @@
+// const x = 0
+// if (true)
+// {
+//      console.log('i am going to be executed')
+// } else
+// {
+//     console.log('i am not going to be executed')
+//  }
+// // console.log(boolean);
+console.log("cat" !== "horse");
+
+if (2 + 2 === 4) {
+  console.log("math is working");
+} else {
+  console.log("math sucks");
+}
+const outOfBread = true;
+const outOfEggs = false;
+
+console.log()
---
+++ .history/block13/index_20240327192829.js
@@ -0,0 +1,27 @@
+// const x = 0
+// if (true)
+// {
+//      console.log('i am going to be executed')
+// } else
+// {
+//     console.log('i am not going to be executed')
+//  }
+// // console.log(boolean);
+console.log("cat" !== "horse");
+
+if (2 + 2 === 4) {
+  console.log("math is working");
+} else {
+  console.log("math sucks");
+}
+const outOfBread = true;
+const outOfEggs = false;
+
+console.log("Time to go shopping");
+if (outOfBread) {
+    console.log("Buy Bread!");    
+}
+if (outOfEggs)
+{
+    console.log("Buy Eggs!");
+    }
