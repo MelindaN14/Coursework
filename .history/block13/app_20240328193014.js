43c8854747defbc2c21e76efa0a578e6a86d782d
MelindaN14 <137628431+MelindaN14@users.noreply.github.com>
2024-03-28 19:31:04 -0400

    a

--- .history/block13/app_20240328193014.js
+++ .history/block13/app_20240328193014.js
@@ -6,6 +6,6 @@
 // console.log(myArray);
 // const animals = ['ant', 'bison', 'camel', 'duck', 'elephant'];
 // console.log(animals.shift);
-console.log('me');
+// console.log('me');
 
 // let number = [10, 20, 30, 40, 50, 60, 70, 80, 90, 100];
