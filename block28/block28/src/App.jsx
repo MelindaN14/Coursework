062d28b7b15acc7a5cf11ca16ba6a4fefa6e70ba
MelindaN14 <137628431+MelindaN14@users.noreply.github.com>
2024-06-04 19:18:35 -0400

    save

---
+++ block28/block28/src/App.jsx
@@ -0,0 +1,12 @@
+import { useState } from "react";
+import "./App.css";
+
+<>
+  <div id="container">
+    <h1>Hello React Router!</h1>
+    <div id="navbar">{/* navigation here */}</div>
+    <div id="main-section">{/* routes here */}</div>
+  </div>
+</>;
+
+export default App;
